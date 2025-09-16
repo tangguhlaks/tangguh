df3c78a4397379da10ae27ef60f78238c91a7e85
TangguhLaksana <tangguh.laksana@nawadata.com>
2025-09-16 13:36:01 +0700

    about

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("pages/home", { name: "Tangguh 🚀" });
+  res.render("pages/home",{ currentPath: req.path });
+});
+
+router.get("/about", (req, res) => {
+  res.render("pages/about",{ currentPath: req.path });
 });
 
 module.exports = router;
